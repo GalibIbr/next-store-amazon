8bfba32bd5f052a14eaa09007a47d2b8d3f74a19
GalibIbr <galibiavi4@gmail.com>
2023-10-10 12:11:02 +0500

    btn products  hover

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -1,9 +1,11 @@
 import { ProductProps } from "../../type";
 import Image from "next/image";
+import { HiShoppingCart } from "react-icons/hi";
+import { FaHeart } from "react-icons/fa";
 
 const Products = ({ productData }: any) => {
   return (
-    <div className="w-full px-6 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap6]">
+    <div className="w-full px-6 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-6">
       {productData.map(
         ({
           _id,
@@ -16,8 +18,27 @@ const Products = ({ productData }: any) => {
           price,
         }: ProductProps) => {
           return (
-            <div id="_id">
-              <Image width={300} height={300} src={image} alt="productImage" />
+            <div
+              key={_id}
+              className="w-full bg-white text-black p-4 border border-gray-300 rounded-lg group overflow-hidden">
+              <div className="w-full h-[260px] relative">
+                <Image
+                  className="w-full h-full object-cover scale-90 hover:scale-100 transition-transform duration-300"
+                  width={300}
+                  height={300}
+                  src={image}
+                  alt="productImage"
+                />
+                <div className="w-12 h-24 absolute bottom-10 right-0 border-[1px] border-gray-400 bg-white rounded-md flex  flex-col translate-x-20 group-hover:translate-x-0 transition-transform duration-300">
+                  <span className="w-full h-full border-b-[1px] border-b-gray-400 flex items-center justify-center text-xl bg-transparent hover:bg-amazon_yellow cursor-pointer duration-300">
+                    <HiShoppingCart />
+                  </span>
+                  <span className="w-full h-full border-b-[1px] border-b-gray-400 flex items-center justify-center text-xl bg-transparent hover:bg-amazon_yellow cursor-pointer duration-300">
+                    <FaHeart />
+                  </span>
+                </div>
+              </div>
+              <hr />
             </div>
           );
         }
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -8,7 +8,7 @@ import Link from "next/link";
 
 const Header = () => {
   return (
-    <div className="w-full h-20 bg-amazon_blue text-lightText sticky top-0">
+    <div className="w-full h-20 bg-amazon_blue text-lightText sticky top-0 z-30">
       <div className="h-full w-full mx-auto inline-flex items-center justify-between gap-1 mdl:gap-3 px-4">
         {/* logo */}
         <Link href={"/"} className="px-2 border border-transparent hover:border-white cursor-pointer duration-300 flex items-center justify-center h-[70%]">
